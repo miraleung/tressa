bb3650a375b177352bb221d3aa93c1df5fb58cfa
Patrick Colp <pjcolp@gmail.com>
2015-07-08 23:41:48 -0700

    Fix get-diffs.sh

--- scripts/get-diffs.sh
+++ scripts/get-diffs.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-cd xen-unstable.hg
+SRC=xen-unstable.hg
+DST=diffs
+
+mkdir -p ${DST}
+
+cd ${SRC}
 
 HEAD=`hg head | grep change | sed s'/changeset:[ ]\+\(.*\):.*/\1/'`
 
-mkdir -p diffs
 
 for rev in `seq 00001 ${HEAD}`
 do
 	zero_led_rev="$(printf "%05d" ${rev})"
 	echo ${zero_led_rev}
-	hg diff -c ${rev} > ../diffs/xen-diff-${zero_led_rev}.patch
+	hg diff -c ${rev} > ../${DST}/xen-diff-${zero_led_rev}.patch
 done
