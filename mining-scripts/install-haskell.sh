30aca78bf3bb7b138cac4fdc2d8806558fcb06d0
Graham St-Laurent <gstlaurent@gmail.com>
2016-06-13 13:02:53 -0700

    Created install-haskell.sh to easily copy necessary files to target repository.
    Also created .gitignore for Haskell artifacts created when compiling and
    deleted obsolete README file.

---
+++ mining-scripts/install-haskell.sh
@@ -0,0 +1,37 @@
+# Copies all necessaray script-mining files to target repository directory
+# (including both get-diffs.sh files).
+
+E_BADARGS=85
+if [[ $# -ne 1 ]]
+then
+    echo "Usage: $(basename $0) <target_repo>"
+    exit $E_BADARGS
+fi
+
+REPO=$1
+
+cp cdf.py git-get-diffs.sh hg-get-diffs.sh $REPO
+cd ./haskell-version
+ghc -O2 GetPredicates.hs -o GetPredicates
+ghc -O2 GetActivity.hs -o GetActivity
+
+printf "Copying scripts to ${REPO} ... "
+cp GetActivity GetPredicates hs-getActivity.sh hs-getPredicates.sh $REPO
+printf "Complete!\n"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
