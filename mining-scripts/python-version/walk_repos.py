953740041e1fbaafb80a2395b43e15e1a949dbc1
Graham St-Laurent <gstlaurent@gmail.com>
2016-07-18 14:40:51 -0700

    Added script walk_repos script for extracting history pickle file for each repo in a directory.

---
+++ mining-scripts/python-version/walk_repos.py
@@ -0,0 +1,32 @@
+# Walks the given repos, storing logs, printouts (.asserts), and pickled 
+# history objects for each.
+
+import logging
+logging.basicConfig(level=logging.DEBUG, filename="walk_repos.log")
+
+import os
+import assertions
+import pickle
+import traceback
+import itertools
+
+
+PICKLE_PROTOCOL = 3 # current default
+
+if __name__ == '__main__':
+    dirs = os.listdir(".")
+    dirs = itertools.filterfalse(lambda d: d.endswith(".py") or d.endswith(".log") or d == 'results', dirs)
+    dirs = list(dirs)
+
+    for i,d in enumerate(dirs):
+        print("{d}   {i}/{n}".format(d=d, i=i+1, n=len(dirs)))
+        try:
+            hist = assertions.mine_repo("\w*(ASSERT|assert|BUG_ON|bug_on)\w*", d, "Tressa")
+            with open('results/' + d + '.pickle', 'wb') as f:
+                pickle.dump(hist, f, PICKLE_PROTOCOL)
+            with open('results/' + d + '.asserts', 'w') as f:
+                for a in hist:
+                    f.write(a.info() + "\n")
+        except:
+            traceback.print_exc()
+
