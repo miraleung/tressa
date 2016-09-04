75aec2d043fe0bf242a8fcd940682d17c41a46b6
Graham St-Laurent <gstlaurent@gmail.com>
2016-09-03 17:16:43 -0700

    Made more useful Contexts repr and str, and created function to easily load Context and print it out for a given pickled history.

--- mining-scripts/python-version/contexts.py
+++ mining-scripts/python-version/contexts.py
@@ -13,6 +13,7 @@ class Contexts():
         Includes the assertname for an example assert in a particular file (however,
         other assertnames may also be available.
         """
+        self.repo = history.repo_path
         self.contexts = []
 
         asserts = sorted(history.assertions(), key=lambda a: a.predicate)
@@ -24,7 +25,8 @@ class Contexts():
                 self.contexts.append(Contexts.Context(name, pred, file, add, rem))
 
     def __repr__(self):
-        return "Contexts" + repr(self.contexts)
+        return "Contexts('{r}', length={l})".format(
+                r=self.repo, l=len(self.contexts))
 
     def __str__(self):
         return self._format()
@@ -32,11 +34,14 @@ class Contexts():
 
     def _format(self, n=None):
         cs = self.contexts if n is None else self.contexts[:n]
-        with StringIO("Contexts[\n") as s:
+        with StringIO() as s:
+            s.write("Contexts[\n")
             for c in cs:
                 s.write("{n}({p}) | {f} | add_id: {a} | rem_id: {r}\n".format(
                     n=c.name, p=c.predicate, f=c.file, a=c.max_add, r=c.max_rem))
+            s.write("]")
             return s.getvalue()
+
 
     def show(self, n=None):
         print(self._format(n))
@@ -59,7 +64,6 @@ def _get_file_asserts(assertions):
     return file_asserts
 
 
-
 def _max_add_rem_ids(asserts):
     """:asserts:    ([Assertions])
     Produce the id of the commit that contains the most assertion adds, and
@@ -76,6 +80,7 @@ def _max_add_rem_ids(asserts):
 
     return add_max[0], rem_max[0]
 
+
 def _add_rem_separate(asserts):
     add_asserts, rem_asserts = [], []
 
@@ -91,3 +96,10 @@ def _add_rem_separate(asserts):
 def _len_iter(it):
     return sum(1 for x in it)
 
+
+def context_show(pickle):
+    """A convenient function for investigating lots of pickled histories"""
+    h = analysis.load_history(pickle)
+    c = Contexts(h)
+    c.show(25)
+    return c
